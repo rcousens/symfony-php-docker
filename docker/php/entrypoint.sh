f42cd720d5980dccc257538adcb0161a1244bd9a
Ross Cousens <ross@hatesmail.com>
2015-06-18 01:10:32 +1000

    Fix order of permission corrections to after composer install

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+echo "env[SYMFONY__DATABASE_HOST]=$POSTGRES_PORT_5432_TCP_ADDR" >> /etc/php5//fpm/pool.d/www.conf
+cd /var/www/html
+SYMFONY__DATABASE_HOST=$POSTGRES_PORT_5432_TCP_ADDR /usr/local/bin/composer install
 rm -fr /var/www/html/app/cache/*
 rm -fr /var/www/html/app/logs/*
 /bin/chmod 777 /var/www/html/app/cache
 /bin/chmod 777 /var/www/html/app/logs
-echo "env[SYMFONY__DATABASE_HOST]=$POSTGRES_PORT_5432_TCP_ADDR" >> /etc/php5//fpm/pool.d/www.conf
-cd /var/www/html
-SYMFONY__DATABASE_HOST=$POSTGRES_PORT_5432_TCP_ADDR /usr/local/bin/composer install
 /usr/sbin/php5-fpm --nodaemonize
