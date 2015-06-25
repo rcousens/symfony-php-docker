87c017844e52fdf0cfd7fe359041d9da35acdfb8
Ross Cousens <ross@hatesmail.com>
2015-06-25 22:44:04 +1000

    Net cat loop to wait for DB ready

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-sleep 10
 cd /var/www/html
 export SYMFONY__DATABASE_HOST=$POSTGRES_PORT_5432_TCP_ADDR 
 export SYMFONY__REDIS_HOST=$REDIS_PORT_6379_TCP_ADDR
 export SYMFONY__ELASTICSEARCH_HOST=$ELASTICSEARCH_PORT_9200_TCP_ADDR
 export SYMFONY_ENV=prod
 /usr/local/bin/composer install
+until nc -z postgres 5432; do
+    echo "$(date) - waiting for postgres..."
+    sleep 1
+done
 /usr/bin/php /var/www/html/app/console doctrine:database:create
 /usr/bin/php /var/www/html/app/console doctrine:schema:create
 rm -fr /var/www/html/app/cache/*
--- docker/worker/entrypoint.sh
+++ docker/worker/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sleep 20
+until nc -z postgres 5432; do
+    echo "$(date) - waiting for postgres..."
+    sleep 1
+done
+sleep 10
 /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
