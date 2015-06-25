98f2bba71bacb0f8288be10f831e0661d698afec
Ross Cousens <ross@hatesmail.com>
2015-06-25 21:44:53 +1000

    Example jobs

--- code/backend/src/AppBundle/Controller/DefaultController.php
+++ code/backend/src/AppBundle/Controller/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
     }
 
     /**
-     * @Route("/app/users", name="homepage")
+     * @Route("/app/users", name="users")
      */
     public function userAction()
     {
@@ -27,4 +27,29 @@ class DefaultController extends Controller
         $em->flush();
         return $this->render('default/index.html.twig');
     }
+
+    /**
+     * @Route("/app/assets", name="assets")
+     */
+    public function assetsAction()
+    {
+        $job = new Job('assets:install');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($job);
+        $em->flush();
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/app/assetic/{env}", name="assetic")
+     */
+    public function asseticAction($env)
+    {
+        $job = new Job('assetic:dump', ["--env=$env"]);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($job);
+        $em->flush();
+        return $this->render('default/index.html.twig');
+    }
+
 }
