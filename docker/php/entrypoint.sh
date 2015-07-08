88789f18e776a1d74ace7c09d7683a3b0a3f5240
Ross Cousens <ross@hatesmail.com>
2015-07-08 23:51:06 +1000

    Mailhog, database user/password auto config

--- docker/php/entrypoint.sh
+++ docker/php/entrypoint.sh
@@ -2,9 +2,13 @@
 
 /bin/sed -i "s@<LOGSTASH_HOST>@${LOGSTASH_1_PORT_514_TCP_ADDR}@" /etc/rsyslog.d/50-default.conf
 
+# move these to php-fpm.conf and drop dependency on composer configuring
 export SYMFONY__DATABASE_HOST=$POSTGRES_PORT_5432_TCP_ADDR
 export SYMFONY__REDIS_HOST=$REDIS_PORT_6379_TCP_ADDR
 export SYMFONY__ELASTICSEARCH_HOST=$ELASTICSEARCH_PORT_9200_TCP_ADDR
+export SYMFONY__MAILER_HOST=${MAILHOG_PORT_1025_TCP_ADDR}:1025
+export SYMFONY__DATABASE_USER=postgres
+export SYMFONY__DATABASE_PASSWORD=today123
 export SYMFONY_ENV=prod
 
 cd /var/www/html
